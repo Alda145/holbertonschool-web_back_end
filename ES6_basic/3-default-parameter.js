312de4050f7c2b616d735b54deb29c8f46a9357d
Alda145 <limiesmeralda@yahoo.com>
2026-01-14 16:55:15 +0100

    Ndryshimet

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,11 +1,9 @@
 
 export default function getSumOfHoods(initialNumber = 34, expansion1989, expansion2019) {
-    if (expansion1989 === undefined) {
-        expansion1989 = 89;
-    }
 
-    if (expansion2019 === undefined) {
-        expansion2019 = 19;
-    }
+    expansion1989 = typeof expansion1989 !== "undefined" ? expansion1989 : 0;
+
+    expansion2019 = typeof expansion2019 !== "undefined" ? expansion2019 : 0;
+
     return initialNumber + expansion1989 + expansion2019;
 }
