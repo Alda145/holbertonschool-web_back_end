189109e21e154ecb24e90dc1ce6b796607ace69b
Alda145 <limiesmeralda@yahoo.com>
2026-01-28 15:26:01 +0100

    Ndryshimet

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -24,6 +24,6 @@ export default class Pricing{
     }
 
     static convertPrice(amount, conversionRate){
-        return this._amount * conversionRate;
+        return amount * conversionRate;
     }
 }
