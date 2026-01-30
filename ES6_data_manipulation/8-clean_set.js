5f18594094415e98ca3f323898c3613b355a534a
Alda145 <limiesmeralda@yahoo.com>
2026-01-30 12:56:34 +0100

    8-main

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, string) {
-  const values = [];
-  for (const x of set.keys()) {
-    if (x.includes(string)) {
-      values.push(x.replace(string, ''));
+    if (string.length === 0) {
+        return "";
     }
-  }
-  return values.join('-');
+    const values = [];
+    for (const x of set.keys()) {
+        if (x.includes(string)) {
+            values.push(x.replace(string, ''));
+        }
+    }
+    return values.join('-');
 }
